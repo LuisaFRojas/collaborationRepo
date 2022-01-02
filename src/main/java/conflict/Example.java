936c1d48932852b9e2d6492adef806b9f01fcd10
LuisaFRojas <66465943+LuisaFRojas@users.noreply.github.com>
2022-01-02 10:18:31 -0700

    Update Example.java

--- src/main/java/conflict/Example.java
+++ src/main/java/conflict/Example.java
@@ -1,4 +1,11 @@
 package conflict;
 
 public class Example {
+      public static void main(String[] args) {
+        System.out.println("make conflict");
+        System.out.println("Hey Hey Hellu Hellu");
+        System.out.println("Hey Hey Hellu Hellu");
+        System.out.println("Hey Hey Hellu Hellu");
+    }
+
 }
