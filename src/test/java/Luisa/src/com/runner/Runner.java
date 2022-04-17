96a64f35ce58d6d4172bf419129e41911a88add7
edward <edwardrexrojas@gmail.com>
2022-04-17 14:05:36 -0600

    pull request commit

---
+++ src/test/java/Luisa/src/com/runner/Runner.java
@@ -0,0 +1,14 @@
+package Luisa.src.com.runner;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/reports/cucumber-report.html",
+        features = "src/test/java/Luisa/resources",
+        glue = "src/test/java/Luisa/src/com/stepDefs",
+        dryRun = false,
+        tags = "@somethingToRun"
+)
+public class Runner {
+}
